987e5371d98b050dbffddbd31f48ca085eef23b6
Shivam Kashyap <shivam.0912.kashyap@gmail.com>
2026-06-10 11:58:36 +0530

    Week 2 Day 3 Solutions

---
+++ Week_2/Day_3/isSubsequence.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if (s.isEmpty()) {
+            return true;
+        }
+        
+        int sPointer = 0;
+        int tPointer = 0;
+        while (tPointer < t.length()) {
+            if (s.charAt(sPointer) == t.charAt(tPointer)) {
+                sPointer++;
+                if (sPointer == s.length()) {
+                    return true;
+                }
+            }
+            tPointer++;
+        }
+        return false;
+    }
+}
+
+  
+    
+
+    
