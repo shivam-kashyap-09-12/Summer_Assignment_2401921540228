a2ba741ac83afed4b5704bff8b1d5457f68e00e5
Shivam Kashyap <shivam.0912.kashyap@gmail.com>
2026-06-14 13:16:28 +0530

    Week 2 OOPS Solution

---
+++ Week_2/OOPS/live/Test.java
@@ -0,0 +1,17 @@
+package live;
+import music.Playable;
+import music.string.Veena;
+import music.wind.Saxophone;
+
+public class Test {
+    public static void main(String[] args) {
+        Veena veena = new Veena();
+        veena.play();
+        Saxophone saxophone = new Saxophone();
+        saxophone.play();
+        Playable p1 = new Veena();
+        p1.play();
+        Playable p2 = new Saxophone();
+        p2.play();
+    }
+}
